3965a277b5500d6d8f9bff1b4508753a831bf0b5
highlyprofitable108 <mikefusc@me.com>
2023-10-24 19:43:36 -0400

    Running with loader and estimate

--- flask_app/static/simulator_input.js
+++ flask_app/static/simulator_input.js
@@ -1,29 +1,75 @@
-function showLoadingSpinner() {
+function validateForm() {
+    // Action Selection
+    const randomHistoricalRadio = document.getElementById('randomHistorical');
+    const nextWeekRadio = document.getElementById('nextWeek');
     const customMatchupsRadio = document.getElementById('customMatchupsRadio');
+
+    if (!randomHistoricalRadio.checked && !nextWeekRadio.checked && !customMatchupsRadio.checked) {
+        alert('Please select an action.');
+        return false;
+    }
+
+    // Simulation Iterations
+    const simIterations = document.getElementById('simIterations').value;
+    if (simIterations && (simIterations < 1 || simIterations > 10000)) {
+        alert('Number of Sim Iterations should be between 1 and 10,000.');
+        return false;
+    }
+
+    // Number of Random Historical Games
+    if (randomHistoricalRadio.checked) {
+        const numRandomGames = document.getElementById('numRandomGames').value;
+        if (!numRandomGames || numRandomGames < 1 || numRandomGames > 500) {
+            alert('Number of Random Historical Games should be between 1 and 500.');
+            return false;
+        }
+    }
+
+    // Custom Matchups
     if (customMatchupsRadio.checked) {
         const homeTeamSelects = document.querySelectorAll('[name^="homeTeam"]');
         const awayTeamSelects = document.querySelectorAll('[name^="awayTeam"]');
         let isAllSelected = true;
+        let selectedTeams = [];
 
         homeTeamSelects.forEach(select => {
             if (!select.value) isAllSelected = false;
+            if (selectedTeams.includes(select.value)) {
+                alert('Each team can only be selected once.');
+                isAllSelected = false;
+            } else {
+                selectedTeams.push(select.value);
+            }
         });
+
         awayTeamSelects.forEach(select => {
             if (!select.value) isAllSelected = false;
+            if (selectedTeams.includes(select.value)) {
+                alert('Each team can only be selected once.');
+                isAllSelected = false;
+            } else {
+                selectedTeams.push(select.value);
+            }
         });
 
         if (!isAllSelected) {
             alert('Please fill out all fields before submitting.');
-            return false; // Do not show the spinner
+            return false;
         }
     }
-    document.getElementById('loading-spinner').style.display = 'block';
-    return true; // Show the spinner
+
+    return true;
 }
 
 document.querySelector('form').addEventListener('submit', function(e) {
-    if (!showLoadingSpinner()) {
-        e.preventDefault(); // Prevent form submission if showLoadingSpinner returns false
+    e.preventDefault();
+
+    if (validateForm()) {
+        showLoadingSpinner();
+
+        setTimeout(function() {
+            e.target.submit();
+        }, 100);
     }
 });
 
@@ -74,41 +120,54 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('customMatchupsRadio').addEventListener('change', function() {
         if (this.checked) {
             document.getElementById('customMatchupsSection').style.display = 'block';
+            document.getElementById('randomHistoricalGamesSection').style.display = 'none';
         }
     });
 
     document.getElementById('randomHistorical').addEventListener('change', function() {
         if (this.checked) {
             document.getElementById('customMatchupsSection').style.display = 'none';
+            document.getElementById('randomHistoricalGamesSection').style.display = 'block';
         }
     });
 
     document.getElementById('nextWeek').addEventListener('change', function() {
         if (this.checked) {
             document.getElementById('customMatchupsSection').style.display = 'none';
+            document.getElementById('randomHistoricalGamesSection').style.display = 'none';
         }
     });
 });
 
-// Show/Hide Custom Matchups Section based on radio button selection
-document.getElementById('customMatchupsRadio').addEventListener('change', function() {
-    if (this.checked) {
-        document.getElementById('customMatchupsSection').style.display = 'block';
-        document.getElementById('randomHistoricalGamesSection').style.display = 'none';
-    }
-});
+function showLoadingSpinner() {
+    const numIterations = document.getElementById('simIterations').value || 1000; // Default to 1 if not provided
+    const randomHistoricalRadio = document.getElementById('randomHistorical');
+    let randomSubset = 16; // Default value
 
-document.getElementById('randomHistorical').addEventListener('change', function() {
-    if (this.checked) {
-        document.getElementById('customMatchupsSection').style.display = 'none';
-        document.getElementById('randomHistoricalGamesSection').style.display = 'block';
+    if (randomHistoricalRadio.checked) {
+        randomSubset = document.getElementById('numRandomGames').value || 1; // Default to 1 if not provided
     }
-});
 
-document.getElementById('nextWeek').addEventListener('change', function() {
-    if (this.checked) {
-        document.getElementById('customMatchupsSection').style.display = 'none';
-        document.getElementById('randomHistoricalGamesSection').style.display = 'none';
-    }
-});
+    // Calculate timeRequired based on the formula
+    let timeRequired = (numIterations * randomSubset) / 5000 * 60;
+    let countdown = timeRequired
 
+    document.getElementById('loading-spinner').style.display = 'block';
+
+    // Calculate the estimated completion time
+    let currentDate = new Date();
+    let completionDate = new Date(currentDate.getTime() + (countdown * 1000)); // Convert seconds to milliseconds
+    
+    let hours = completionDate.getHours();
+    let minutes = completionDate.getMinutes().toString().padStart(2, '0');
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    
+    let completionTime = `${hours}:${minutes} ${ampm}`;
+    
+    // Display the estimated completion time
+    document.getElementById('countdown-timer').textContent = "Estimated completion time: " + completionTime;
+
+    return true; // Show the spinner
+}
